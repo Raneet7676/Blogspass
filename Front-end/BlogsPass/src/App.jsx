ac83c8cf5639210c0d7868fed8b39f692760e179
Raneet <raneetroy0905@gmail.com>
2026-01-29 17:13:44 +0530

    router is initialised for routes

--- Front-end/BlogsPass/src/App.jsx
+++ Front-end/BlogsPass/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import PreviewPage from './pages/preview-page/PreviewPage';
+import { Route , Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <PreviewPage/>
+      <Routes>
+        <Route path='/' element={<PreviewPage/>}/>
+      </Routes>
     </>
   )
 }
